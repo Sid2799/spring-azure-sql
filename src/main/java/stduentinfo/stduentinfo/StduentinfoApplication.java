2252dde302de7ee9a82e1b5340a5a8bfa2cb8adf
Sid2799 <prembankar2706@gmail.com>
2022-06-24 07:37:07 +0530

    first

---
+++ src/main/java/stduentinfo/stduentinfo/StduentinfoApplication.java
@@ -0,0 +1,13 @@
+package stduentinfo.stduentinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StduentinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StduentinfoApplication.class, args);
+	}
+
+}
